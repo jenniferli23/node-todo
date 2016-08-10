0c3457774bd1df03a627650b78ddb584b83047ba
Jennifer <jennifer.li@appdynamics.com>
2016-08-09 22:06:10 -0700

    Edit remote

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://heroku_kwcshbh8:kmsakn70nvrecafgjslpm1e6j7@ds153705.mlab.com:53705/heroku_kwcshbh8',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
